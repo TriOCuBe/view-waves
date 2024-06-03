81ae1434dd8d39d0c2cbdd37016193db8cf0661d
TriOCuBe <crimeblocky73@gmail.com>
2024-06-03 21:23:48 +0200

    Revamp

--- scripts/display-enemies.js
+++ scripts/display-enemies.js
@@ -1,17 +1,17 @@
-Events.on(DepositEvent, event => {
-	var spawns = Vars.spawner.countSpawns();
-	const myDialog = new BaseDialog("Title");
-	Vars.state.rules.spawns.forEach(function(group) {
-		if (group.type == null) return;
+// Events.on(DepositEvent, event => {
+// 	var spawns = Vars.spawner.countSpawns();
+// 	const myDialog = new BaseDialog("Title");
+// 	Vars.state.rules.spawns.forEach(function(group) {
+// 		if (group.type == null) return;
 
-		let spawned = group.getSpawned(Vars.state.wave - 1) * spawns;
-		if (spawned == 0) return;
-		myDialog.cont.add(group.type.toString() + " " + spawned.toString() + " (flying: " + group.type.flying + ")");
-		myDialog.cont.add("\n");
-	});
-	myDialog.addCloseButton();
-	myDialog.show();
-})
+// 		let spawned = group.getSpawned(Vars.state.wave - 1) * spawns;
+// 		if (spawned == 0) return;
+// 		myDialog.cont.add(group.type.toString() + " " + spawned.toString() + " (flying: " + group.type.flying + ")");
+// 		myDialog.cont.add("\n");
+// 	});
+// 	myDialog.addCloseButton();
+// 	myDialog.show();
+// })
 
 const { ImageButtonStyle } = ImageButton;
 const { TextButtonStyle } = TextButton;
@@ -56,50 +56,96 @@ Events.on(ClientLoadEvent, () => {
 });
 
 function displayNextWave() {
-	var spawners = Vars.spawner.countSpawns();
-	var wavesDialog = new BaseDialog("Next Wave: " + Vars.state.wave);
-	// we have a winWave
-	var minWave = Vars.state.wave;
-	if (Vars.state.rules.winWave > 0) {
-		var maxWave = Vars.state.rules.winWave;
-	} else { // no winWave, just show a range of 25 waves
-		var maxWave = minWave + 25;
-	}
-	if (maxWave - minWave < 25) minWave = maxWave - 25;
-	if (minWave < 0) minWave = 1;
-	for (let i = minWave; i < maxWave; ++i) {
-		if (i < Vars.state.wave) {
-			var color = Color.gray;
-		} else if (i == Vars.state.wave) {
-			var color = Color.valueOf("ffd37f");
-		} else {
-			var color = Color.white;
-		}
-		wavesDialog.cont.table(null, table => {
-			table.defaults().pad(10)
-			table.add("Wave " + i).color(color);
-			table.row();
-			
-			Vars.state.rules.spawns.forEach(function(group) {
-				if (group.type == null) return;
+	const currentWave = Vars.state.wave;
+	const winWave = Vars.state.rules.winWave;
+	const spawners = Vars.spawner.countSpawns();
+	const wavesDialog = new BaseDialog("Next Wave: " + currentWave);
 
-				let spawned = group.getSpawned(i) * spawners;
-				if (spawned == 0) return;
-
-				let info = "type: ";
-				if (group.type.flying) info = info.concat("flying");
-				if (group.type.hovering) info = info.concat("hovering");
-				if (group.type.naval) info = info.concat("naval");
-				if (info.length == 6) info = info.concat("ground");
-
-				table.add(group.type + " " + spawned + "         " + info).color(color);
-				table.row();
-			});
-		}).grow();
-		if ((i - minWave + 1) % 5 == 0) {
-			wavesDialog.cont.row();
-		};
+	if (spawners == 0) {
+		wavesDialog.cont.add("This sector has no waves.");
+		wavesDialog.addCloseButton();
+		wavesDialog.show();
+		return;
 	};
+	
+	// displayedWaves only matters for endless sectors
+	const displayedWaves = 100;
+	const displayedColumns = 4;
+
+	var minWave = currentWave;
+	// we have a winWave
+	if (winWave > 0) {
+		var maxWave = winWave + 1;
+	// no winWave, just show a range of displayedWaves waves
+	} else {
+		var maxWave = minWave + displayedWaves;
+		if (maxWave - minWave < displayedWaves) minWave = maxWave - displayedWaves;
+		if (minWave < 0) minWave = 1;
+	}
+	// this enables i = -1, so that we can display the endless message
+	if (winWave <= 0) minWave -= 1;
+
+	wavesDialog.cont.pane(p => {
+		// offset -1 for waves, winWave 40 means 39 is the last wave
+		for (let i = minWave - 1; i < maxWave - 1; ++i) {
+			if (i == -1) {
+				p.table(null, table => {
+					table.add("You are playing in endless mode. Since there is no final wave,").color(Color.valueOf("a6fcaf")).row();
+					table.add("you can simply see the next 100 waves here. Have fun.").color(Color.valueOf("a6fcaf")).row();
+					return;
+				}).colspan(4).grow().padBottom(10);
+				p.row();
+				continue;
+			};
+
+			// set text color
+			if (i < currentWave - 1) {
+				var color = Color.gray;
+			} else if (i == currentWave - 1) {
+				var color = Color.valueOf("ffd37f");
+			} else {
+				var color = Color.white;
+			}
+
+			p.table(null, table => {
+				table.top();
+				table.defaults().pad(7);
+				if (i >= 0) table.add("Wave " + (i+1)).color(color);
+				table.row();
+
+				// displays this message instead of wave units on winWave
+				if (winWave && (i + 1) == winWave) {
+					table.add("Captured Sector! :)").color(Color.valueOf("a6fcaf"));
+					return;
+				};
+
+				Vars.state.rules.spawns.forEach(function(group) {
+					if (group.type == null) return;
+
+					let spawned = group.getSpawned(i) * spawners;
+					if (spawned == 0) return;
+
+					let info = "type: ";
+					if (group.type.flying) info = info.concat("flying");
+					if (group.type.hovering) info = info.concat("hovering");
+					if (group.type.naval) info = info.concat("naval");
+					if (info.length == 6) info = info.concat("ground");
+
+					// mark bosses
+					if (group.effect == StatusEffects.boss) color = Color.valueOf("fc585b");
+
+					table.add(group.type + " " + spawned).left().color(color);
+					table.add(info).right().color(color);
+					table.row();
+				});
+			}).grow().padBottom(25);
+			
+			if (minWave < 1) minWave = 1;
+			if ((i + 1 - minWave + 1) % displayedColumns == 0 && (i + 1 - minWave + 1) != 0) {
+				p.row();
+			};
+		};
+	}).grow();
 	wavesDialog.addCloseButton();
 	wavesDialog.show();
-}
+};
